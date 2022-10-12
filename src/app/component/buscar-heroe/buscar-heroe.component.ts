eefc2b74aef194be0e3159cd997a19ec62873cc1
JJMPantoja <pantojam48@gmail.com>
2022-10-12 03:05:35 -0500

    Arreglo de elementos visuales

--- src/app/component/buscar-heroe/buscar-heroe.component.ts
+++ src/app/component/buscar-heroe/buscar-heroe.component.ts
@@ -2,29 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HeroesService } from '../../services/heroes.service';
 
-
-
 @Component({
   selector: 'app-buscar-heroe',
   templateUrl: './buscar-heroe.component.html',
-  styleUrls: ['./buscar-heroe.component.css']
+  styleUrls: ['./buscar-heroe.component.css'],
 })
 export class BuscarHeroeComponent implements OnInit {
-
   buquedaHeroesResul: any[] = [];
   termino: string;
 
-  constructor(private service: HeroesService,
-              private activatedRoute: ActivatedRoute,
-              ) { }
+  constructor(
+    private service: HeroesService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-
     this.activatedRoute.params.subscribe((params) => {
       this.buquedaHeroesResul = this.service.buscarHeroe(params.name);
       this.termino = params.name;
-      console.log(this.buquedaHeroesResul);
     });
   }
-
 }
